faf1d4167eb7a6c6762c90f507d4f227a747074e
Eric Santana <efms.king@gmail.com>
2024-08-16 18:36:33 -0300

    feat: user DTO

--- apps/api/src/dtos/user/user.dto.ts
+++ apps/api/src/dtos/user/user.dto.ts
@@ -1,29 +1,62 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsOptional, IsString } from 'class-validator';
 
 export class User {
     @ApiProperty()
+    @IsString()
+    @IsOptional()
     _id?: string;
 
     @ApiProperty()
+    @IsString()
+    @IsOptional()
     username?: string
 
     @ApiProperty()
+    @IsString()
     email: string
 
     @ApiProperty()
-    password?: string
+    @IsString()
+    password: string
+
+    @ApiProperty({required: false, description: "Last time the user logged in the system."})
+    @IsString()
+    @IsOptional()
+    lastLoginAt?: string
+
+    @ApiProperty({required: false, readOnly: true})
+    @IsString()
+    @IsOptional()
+    readonly createdAt?: string
+
+    @ApiProperty({required: false, readOnly: true})
+    @IsString()
+    @IsOptional()
+    readonly updatedAt?: string
 
     @ApiProperty()
-    status?: string
+    @Type(() => OAuth)
+    oAuth?: OAuth
+   
+}
+
+class OAuth {
 
     @ApiProperty()
-    profileId?: string
+    @IsString()
+    provider: string
 
     @ApiProperty()
-    createdAt?: string
+    @IsString()
+    providerId: string
 
     @ApiProperty()
-    updatedAt?: string
+    @IsString()
+    accessToken: string
 
-    
+    @ApiProperty()
+    @IsString()
+    refreshToken: string
 }
